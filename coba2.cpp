e167a2915f7798d53be208ef35f72d8c10538068
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-04-28 19:14:15 +0700

    prosedur input

--- coba2.cpp
+++ coba2.cpp
@@ -6,5 +6,13 @@ class PersegiPanjang {
        float panjang;
        float lebar;
        float luas;
+   
+       void inputData(){
+        cout << "input panjang : ";
+        cin >> panjang;
+        cout << "input lebar : ";
+        cin >> lebar;
+       }
+
        
 };
